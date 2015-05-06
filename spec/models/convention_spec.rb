ac2d2b8d85a4a1ae09b6ff2579607a9ee4200fbc
James Simshaw <james.simshaw@gmail.com>
2015-05-06 12:57:37 -0700

    Created factories_spec to test if factory is valid

--- spec/models/convention_spec.rb
+++ spec/models/convention_spec.rb
@@ -1,10 +1,6 @@
 require 'rails_helper'
 
 describe Convention do
-  it "has a valid factory" do
-    expect(FactoryGirl.build(:convention)).to be_valid
-  end
-
   it "is invalid without a name" do
     expect(FactoryGirl.build(:convention, name: nil)).not_to be_valid
   end
