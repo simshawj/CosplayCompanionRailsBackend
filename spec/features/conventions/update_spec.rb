3fea611dd69c84aef4bde15dcf34ac4d7aadf07a
James Simshaw <james.simshaw@gmail.com>
2015-05-27 12:55:30 -0700

    Refactored conventions update_spec

--- spec/features/conventions/update_spec.rb
+++ spec/features/conventions/update_spec.rb
@@ -1,15 +1,24 @@
 require 'rails_helper'
 
+def edit_convention(options={})
+  con = create(:convention)
+
+  options[:name] ||= con.name
+  options[:description] ||= con.description
+  options[:address] ||= "conventions/" + con.id.to_s
+
+  visit options[:address]
+  click_link "Edit"
+
+  fill_in "Name", with: options[:name]
+  fill_in "Description", with: options[:description]
+  click_button "Submit"
+end
+
 describe "updating conventions" do
   context "valid convention" do
     it "updates a convention" do
-      con = create(:convention)
-
-      visit("conventions/" + con.id.to_s)
-      click_link "Edit"
-
-      fill_in "Name", with: "A changed convention"
-      click_button "Submit"
+      edit_convention(name: "A changed convention")
 
       expect(page).to have_content "Convention updated"
       expect(page).to have_content "A changed convention"
@@ -18,14 +27,7 @@ describe "updating conventions" do
 
   context "invalid convention" do
     it "does not update the convention" do
-      con = create(:convention)
-      name = con.name
-
-      visit("conventions/" + con.id.to_s)
-      click_link "Edit"
-
-      fill_in "Name", with: ""
-      click_button "Submit"
+      edit_convention(name: "")
 
       expect(page).to have_content "Failed to update convention"
       expect(page).to have_content "Name can't be blank"
