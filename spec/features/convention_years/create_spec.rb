89f80d4733c9572af71d161ae22102f650f2fc9d
James Simshaw <james.simshaw@gmail.com>
2015-05-27 13:59:31 -0700

    Refactored convention_years create_spec

--- spec/features/convention_years/create_spec.rb
+++ spec/features/convention_years/create_spec.rb
@@ -1,41 +1,40 @@
 require 'rails_helper'
 
-def fill_in_new_convention_year(convention)
-  select convention.name, from: "Convention"
-  fill_in "Year", with: "2015"
-  fill_in "Number of Days", with: "5"
-  select "May", from: "convention_year_start_2i"
-  select "22", from: "convention_year_start_3i"
-  select "2015", from: "convention_year_start_1i"
+def fill_in_new_convention_year(options={})
+  options[:year] ||= "2015"
+  options[:days] ||= "5"
+  options[:month] ||= "May"
+  options[:day] ||= "22"
+
+  visit "convention_years"
+  click_link "Add a new year"
+
+  select options[:convention].name, from: "Convention"
+  fill_in "Year", with: options[:year]
+  fill_in "Number of Days", with: options[:days]
+  select options[:month], from: "convention_year_start_2i"
+  select options[:day], from: "convention_year_start_3i"
+  select options[:year], from: "convention_year_start_1i"
   click_button "Submit"
 
-  expect(page).to have_content convention.name
-  expect(page).to have_content "2015"
-  expect(page).to have_content "Convention year successfully created"
 end
 
 describe "Convention Year Creation" do
   let!(:convention) { create(:convention) }
   context "with a valid parameters" do
     it "creates the convention_year" do
-      visit("convention_years/new")
-
-      fill_in_new_convention_year(convention)
-    end
-    it "creates a convention_year when starting from the index" do
-      visit("convention_years")
-      click_link("Add a new year")
-      fill_in_new_convention_year(convention)
+      fill_in_new_convention_year convention: convention 
+      
+      expect(page).to have_content convention.name
+      expect(page).to have_content "2015"
+      expect(page).to have_content "Convention year successfully created"
     end
   end
 
   context "with no paramters" do
     it "does not create a convention" do
-      visit("convention_years/new")
-      click_button "Submit"
+      fill_in_new_convention_year convention: convention, days: "" 
 
-      expect(page).to have_content "Year can't be blank"
-      expect(page).to have_content "Year is not a number"
       expect(page).to have_content "Days can't be blank"
       expect(page).to have_content "Days is not a number"
     end
