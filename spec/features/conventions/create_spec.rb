318d8b0c79b4d1fa7d9a22e2b1bd9ce361261dd5
James Simshaw <james.simshaw@gmail.com>
2015-05-27 12:18:43 -0700

    Refactored conventions create_spec

--- spec/features/conventions/create_spec.rb
+++ spec/features/conventions/create_spec.rb
@@ -1,31 +1,26 @@
 require 'rails_helper'
 
-def create_convention
-    fill_in "Name", with: "A Test Convention"
-    fill_in "Description", with: "A nonexistant convention that is used to test"
-    click_button "Submit"
+def create_convention(options={})
+  options[:name] ||= "A Test Convention"
+  options[:description] ||= "A nonexistant convention that is used to test"
 
-    expect(page).to have_content "Conventions"
-    expect(page).to have_content "Convention successfully created"
+  visit "conventions"
+  click_link "Add a new convention"
+
+  fill_in "Name", with: options[:name]
+  fill_in "Description", with: options[:description]
+  click_button "Submit"
+
 end
 
 describe "Convention creation" do
-  it "creates a new convention from conventions/new" do
-    visit("conventions/new")
-    create_convention
-  end
-
   it "creates a new convention from conventions" do
-    visit("conventions")
-    click_link "Add a new convention"
     create_convention
+    expect(page).to have_content "Convention successfully created"
   end
 
   it "displays an error with no name" do
-    visit("conventions/new")
-    fill_in "Description", with: "A nonexistant convention used to test"
-    click_button "Submit"
-
+    create_convention(name: "")
     expect(page).to have_content "Name can't be blank"
   end
 end
