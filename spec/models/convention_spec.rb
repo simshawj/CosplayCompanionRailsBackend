4cc6ac364584128dc39b38c2aa66ff75dfed929b
James Simshaw <james.simshaw@gmail.com>
2015-05-27 07:55:23 -0700

    Updated convention_spec to not require FactoryGirl classname

--- spec/models/convention_spec.rb
+++ spec/models/convention_spec.rb
@@ -2,16 +2,16 @@ require 'rails_helper'
 
 describe Convention do
   it "is invalid without a name" do
-    expect(FactoryGirl.build(:convention, name: nil)).not_to be_valid
+    expect(build_stubbed(:convention, name: nil)).not_to be_valid
   end
 
   it "is invalid with an empty name" do
-    expect(FactoryGirl.build(:convention, name: "")).not_to be_valid
+    expect(build_stubbed(:convention, name: "")).not_to be_valid
   end
 
   it "is unique" do
-    FactoryGirl.create(:convention)
-    expect(FactoryGirl.build(:convention)).not_to be_valid
+    create(:convention)
+    expect(build_stubbed(:convention)).not_to be_valid
   end
 
   it { should have_many(:convention_years) }
