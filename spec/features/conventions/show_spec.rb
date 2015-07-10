51bf57acfce8af69c9703231bf88975cf8df402d
James Simshaw <james.simshaw@gmail.com>
2015-07-10 16:47:27 -0700

    List of convention years now takes place in the convention show page

--- spec/features/conventions/show_spec.rb
+++ spec/features/conventions/show_spec.rb
@@ -1,8 +1,12 @@
 require 'rails_helper'
 
 describe "Show Convention" do
-  it "displays the information about the convention" do
+  it "displays the information about the convention including years" do
     con = create(:convention)
+    con2 = create(:convention, name: "Convention 2")
+
+    con_year = create(:convention_year, year: 2012, convention: con)
+    con2_year = create(:convention_year, year: 2010, convention: con2)
 
     visit("conventions")
 
@@ -10,5 +14,7 @@ describe "Show Convention" do
     click_link con.name
     expect(page).to have_content con.name
     expect(page).to have_content con.description
+    expect(page).to have_content con_year.year
+    expect(page).not_to have_content con2_year.year
   end
 end
