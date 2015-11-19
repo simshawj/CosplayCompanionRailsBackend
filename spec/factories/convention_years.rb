34e5b1ba95553a00bee6e226d86b8f0c838d61ec
James Simshaw <james.simshaw@gmail.com>
2015-11-19 08:04:58 -0800

    Fixed factories so that you could link multiple years to one convention

--- spec/factories/convention_years.rb
+++ spec/factories/convention_years.rb
@@ -3,15 +3,15 @@ FactoryGirl.define do
     date = Date.strptime("10-20-2015", "%m-%d-%Y")
     year 2015
     start date
-    convention
     finish (date + 4.days)
+    convention { Convention.first || FactoryGirl.create(:convention) }
   end
 
   factory :convention_year2, class: ConventionYear do
     date = Date.strptime("10-20-2016", "%m-%d-%Y")
     year 2016
     start date
-    convention
     finish (date + 3.days)
+    convention { Convention.first || FactoryGirl.create(:convention) }
   end
 end
--- spec/factories/conventions.rb
+++ spec/factories/conventions.rb
@@ -2,7 +2,6 @@ FactoryGirl.define do
   factory :convention do
     name "Test Convention"
     description "This is a test convention."
-    initialize_with { Convention.where(name: name).first_or_create }
   end
 end
 
