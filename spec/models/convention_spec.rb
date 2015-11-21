d027a9a74b79d55ba02f83daa8bc4b98d54e3b27
James Simshaw <james.simshaw@gmail.com>
2015-11-21 10:11:53 -0800

    Converted some of convention's model specs to shoulda matchers

--- spec/models/convention_spec.rb
+++ spec/models/convention_spec.rb
@@ -1,18 +1,9 @@
 require 'rails_helper'
 
 describe Convention do
-  it "is invalid without a name" do
-    expect(build_stubbed(:convention, name: nil)).not_to be_valid
-  end
+  it { is_expected.to validate_presence_of(:name) }
 
-  it "is invalid with an empty name" do
-    expect(build_stubbed(:convention, name: "")).not_to be_valid
-  end
-
-  it "is unique" do
-    create(:convention)
-    expect(build_stubbed(:convention)).not_to be_valid
-  end
+  it { is_expected.to validate_uniqueness_of(:name) }
 
   it { should have_many(:convention_years) }
 
