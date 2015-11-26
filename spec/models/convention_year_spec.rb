1cad68bf093b266eefab24313ed05628e9b7e59b
James Simshaw <james.simshaw@gmail.com>
2015-11-26 10:55:46 -0800

    Updated convention_year_spec to use more shoulda matchers

--- spec/models/convention_year_spec.rb
+++ spec/models/convention_year_spec.rb
@@ -1,22 +1,22 @@
 require 'rails_helper'
 
 describe ConventionYear do
-  it "is invalid without a location" do
-    expect(build_stubbed(:convention_year, location: nil)).not_to be_valid
-  end
+  it { is_expected.to validate_presence_of(:location) }
 
-  it "is invalid without a start date" do
-    expect(build_stubbed(:convention_year, start: nil)).not_to be_valid
-  end
+  it { is_expected.to validate_presence_of(:start) }
 
+  it { is_expected.to validate_presence_of(:finish) }
+
+  it { is_expected.to validate_presence_of(:convention) }
+
+  it { should belong_to(:convention) }
+
+  it { should have_many(:photo_shoots) }
+  
   it "is invalid if start date is not a date object" do
     expect(build_stubbed(:convention_year, start: "A string")).not_to be_valid
     expect(build_stubbed(:convention_year, start: 342)).not_to be_valid
     expect(build_stubbed(:convention_year, start: 3.14)).not_to be_valid
-  end
-
-  it "is invalid without a finish date" do
-    expect(build_stubbed(:convention_year, finish: nil)).not_to be_valid
   end
 
   it "is invalid if finish date is not a date object" do
@@ -28,14 +28,4 @@ describe ConventionYear do
   it "is invalid if it starts after it ends" do
     expect(build_stubbed(:convention_year, finish: Date.strptime("10-19-2015", "%m-%d-%Y"))).not_to be_valid
   end
-
-  it { should belong_to(:convention) }
-
-  it "is invalid without a convention_id" do
-    expect(build_stubbed(:convention_year, convention: nil)).not_to be_valid
-  end
-
-  it { should have_many(:photo_shoots) }
-
 end
-
