4e09083a088239c11c70609c8f9d2ca97321bbbd
James Simshaw <james.simshaw@gmail.com>
2015-11-30 15:20:18 -0800

    Made the factory the bare bones of what is required by devise

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,9 +1,6 @@
 FactoryGirl.define do
   factory :user do
-    username "johndoe"
     email "john.doe@someplace.com"
     password "Insecure"
-    password_confirmation "Insecure"
-    confirmed_at Time.now
   end
 end
