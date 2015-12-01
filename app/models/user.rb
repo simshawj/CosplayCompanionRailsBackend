2e4495d0f3f8130b3039f737bfac30e8bb667e2a
James Simshaw <james.simshaw@gmail.com>
2015-11-30 17:48:48 -0800

    Made usernames unique

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  validates :username, presence: true, uniqueness: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,6 @@
 FactoryGirl.define do
   factory :user do
+    username "johndoe"
     email "john.doe@someplace.com"
     password "Insecure"
   end
