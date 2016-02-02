fe43d4b529248850b631d6e85256c677e3371435
James Simshaw <james.simshaw@gmail.com>
2016-02-01 18:18:18 -0800

    Removed convention_year views

--- app/controllers/convention_years_controller.rb
+++ app/controllers/convention_years_controller.rb
@@ -7,28 +7,18 @@ class ConventionYearsController < ApplicationController
       @convention_years = ConventionYear.all.order(:convention_id)
     end
     respond_to do |format|
-      format.html {}
       format.json { render json: @convention_years }
     end
   end
-
-  def new
-    @convention_year = ConventionYear.new
-  end
-
+  
   def create
     @convention_year = ConventionYear.new(convention_year_params)
     if @convention_year.save
       respond_to do |format|
-        format.html { redirect_to convention_years_path, success: "Convention year successfully created" }
         format.json { render json: @convention_year, status: 201 }
       end
     else
       respond_to do |format|
-        format.html do
-          flash[:alert] = "Could not create convention year"
-          render new_convention_year_path
-        end
         format.json { render json: { errors: @convention_year.errors.full_messages }, status: 422 }
       end
     end
@@ -42,27 +32,14 @@ class ConventionYearsController < ApplicationController
     end
   end
 
-  def edit
-    begin
-      @convention_year = ConventionYear.find(params[:id])
-    rescue
-      redirect_to convention_years_path, alert: "Could not retrieve convention year to edit"
-    end
-  end
-
   def update
     @convention_year = ConventionYear.find(params[:id])
     if @convention_year.update(convention_year_params)
       respond_to do |format|
-        format.html { redirect_to convention_years_path, success: "Convention year successfully updated" }
         format.json { render json: @convention_year, status: 200 }
       end
     else
       respond_to do |format|
-        format.html do
-          flash[:alert] = "Unable to update convention year"
-          render :edit
-        end
         format.json { render json: { errors: @convention_year.errors.full_messages }, status: 422 }
       end
     end
--- spec/controllers/convention_years_controller_spec.rb
+++ spec/controllers/convention_years_controller_spec.rb
@@ -9,24 +9,6 @@ describe ConventionYearsController do
   let(:second_attribs) { attributes_for(:convention_year, finish: Date.today + 7.days).merge(convention_id: convention.id) }
 
   describe "GET #index" do
-    it "renders the :index view" do
-      get :index
-      expect(response).to render_template :index
-    end
-    it "creates a list of convention years" do
-      get :index
-      expect(assigns(:convention_years)).to eq([convention_year])
-    end
-    it "sorts by convention id then by year" do
-      con2 = create(:convention, name: "Con2")
-      convention_year2 = create(:convention_year2, convention: convention)
-      con2year1 = create(:convention_year, convention: con2)
-      con2year2 = create(:convention_year2, convention: con2)
-      get :index
-      #TODO: Clean this up if possible
-      array = [convention_year2, convention_year, con2year1, con2year2]
-      expect(assigns(:convention_years)).to eq(array)
-    end
     context "as JSON" do
       before(:each) do
         convention_year
@@ -44,45 +26,7 @@ describe ConventionYearsController do
     end
   end
 
-  describe "GET #new" do
-    before(:each) { get :new }
-    it "renders the :new view" do
-      expect(response).to render_template :new
-    end
-    it "creates a new convention year" do
-      expect(assigns(:convention_year)).to be_a_new(ConventionYear)
-    end
-  end
-
   describe "POST #Create" do
-    context "with a valid convention year" do
-      it "redirects to the :index view" do
-        post :create, convention_year: convention_year_attribs
-        expect(response).to redirect_to convention_years_path
-      end
-      it "saves a convention year" do
-        expect{post :create, convention_year: convention_year_attribs}.to change{ConventionYear.count}.by(1)
-      end
-      it "sets flash success message" do
-        post :create, convention_year: convention_year_attribs
-        expect(flash[:success]).to be_present
-      end
-    end
-
-    context "with an invalid convention year" do
-      it "redirects to the :new view" do
-        post :create, convention_year: invalid_attribs
-        expect(response).to render_template "new"
-      end
-      it "does not save the convnetion year" do
-        expect{post :create, convention_year: invalid_attribs}.not_to change{ConventionYear.count}
-      end
-      it "sets flash alert message" do
-        post :create, convention_year: invalid_attribs
-        expect(flash[:alert]).to be_present
-      end
-    end
-
     context "as JSON" do
       context "with valid parameters" do
         it "returns a status code of 201" do
@@ -100,16 +44,6 @@ describe ConventionYearsController do
   end
 
   describe "GET #show" do
-    context "with a valid id" do
-      before(:each) { get :show, id: id }
-      it "renders the :show view" do
-        expect(response).to render_template :show
-      end
-      it "retrieves a convention year" do
-        expect(assigns(:convention_year)).to eq(convention_year)
-      end
-    end
-
     context "with an invalid id" do
       before(:each) do
         convention_year.delete
@@ -125,64 +59,7 @@ describe ConventionYearsController do
     end
   end
 
-  describe "GET #edit" do
-    context "with a valid id" do
-      before(:each) { get :edit, id: id }
-      it "renders the :edit view" do
-        expect(response).to render_template :edit
-      end
-      it "retrieves a convention year" do
-        expect(assigns(:convention_year)).to eq(convention_year)
-      end
-    end
-
-    context "with an invalid id" do
-      before(:each) do
-        convention_year.delete
-        get :edit, id: id
-      end
-      it "redirects to the :index view" do
-        expect(response).to redirect_to convention_years_path
-      end
-      it "sets flash alert message" do
-        expect(flash[:alert]).to be_present
-      end
-    end
-  end
-
   describe "PUT #update" do
-    context "with valid attributes" do
-      it "redirects to the :index view" do
-        put :update, id: id, convention_year: second_attribs
-        expect(response).to redirect_to convention_years_path
-      end
-      it "modifies the convention year" do
-        put :update, id: id, convention_year: second_attribs
-        convention_year.reload
-        expect(convention_year.finish).to eq(Date.today + 7.days)
-      end
-      it "sets a successful flash message" do
-        put :update, id: id, convention_year: second_attribs
-        expect(flash[:success]).to be_present
-      end
-    end
-
-    context "with invalid attributes" do
-      it "render to the :edit view" do
-        put :update, id: id, convention_year: invalid_attribs
-        expect(response).to render_template :edit
-      end
-      it "does not modify a convention year" do
-        put :update, id: id, convention_year: invalid_attribs
-        convention_year.reload
-        expect(convention_year.convention_id).to_not be_nil
-      end
-      it "sets an alert flash message" do
-        put :update, id:id, convention_year: invalid_attribs
-        expect(flash[:alert]).to be_present
-      end
-    end
-
     context "using JSON" do
       context "with valid attributes" do
         it "responds with a 200 status" do
