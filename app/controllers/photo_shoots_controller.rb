48fb0cab26595ddc7c4313aa5d311237a9de5c50
James Simshaw <james.simshaw@gmail.com>
2016-02-01 19:21:34 -0800

    Removed photo shoot views and fixed specs

--- app/controllers/photo_shoots_controller.rb
+++ app/controllers/photo_shoots_controller.rb
@@ -7,28 +7,18 @@ class PhotoShootsController < ApplicationController
       @photo_shoots = PhotoShoot.all
     end
     respond_to do |format|
-      format.html {}
       format.json { render json: @photo_shoots }
     end
-  end
-
-  def new
-    @photo_shoot = PhotoShoot.new
   end
 
   def create
     @photo_shoot = PhotoShoot.new(photo_shoot_params)
     if @photo_shoot.save
       respond_to do |format|
-        format.html { redirect_to photo_shoots_path, success: "Photo shoot successfully created" }
         format.json { render json: @photo_shoot, status: 201 }
       end
     else
       respond_to do |format|
-        format.html do
-          flash[:alert] = "Could not create photo shoot"
-          render new_photo_shoot_path
-        end
         format.json { render json: { errors: @photo_shoot.errors.full_messages }, status: 422 }
       end
     end
@@ -42,27 +32,14 @@ class PhotoShootsController < ApplicationController
     end
   end
 
-  def edit
-    begin
-      @photo_shoot = PhotoShoot.find(params[:id])
-    rescue
-      redirect_to photo_shoots_path, alert: "Could not find specified photo shoot"
-    end
-  end
-
   def update
     @photo_shoot = PhotoShoot.find(params[:id])
     if @photo_shoot.update(photo_shoot_params)
       respond_to do |format|
-        format.html { redirect_to photo_shoots_path, success: "Photo shoot successfully updated" }
         format.json { render json: @photo_shoot, status: 200 }
       end
     else
       respond_to do |format|
-        format.html do
-          flash[:alert] = "Could not update photo shoot"
-          render :edit
-        end
         format.json { render json: { errors: @photo_shoot.errors.full_messages }, status: 422 }
       end
     end
--- spec/controllers/photo_shoots_controller_spec.rb
+++ spec/controllers/photo_shoots_controller_spec.rb
@@ -9,20 +9,6 @@ describe PhotoShootsController do
   let(:id) { photo_shoot.id }
 
   describe "GET #index" do
-    before(:each) { get :index }
-    it "renders the :index view" do 
-      expect(response).to render_template :index
-    end
-    it "creates a list of photoshoots" do
-      expect(assigns(:photo_shoots)).to eq([photo_shoot])
-    end
-    it "returns only photoshoots for a single year when one is given" do
-      year2 = create(:convention_year2)
-      shoot2 = create(:photo_shoot, convention_year: year2, start: DateTime.strptime("10-22-2016", "%m-%d-%Y"))
-      get :index, convention_year_id: convention_year.id
-      expect(assigns(:photo_shoots)).to eq([photo_shoot])
-    end
-
     context "as json" do
       before(:each) do
         photo_shoot
@@ -37,45 +23,7 @@ describe PhotoShootsController do
     end
   end
 
-  describe "GET #new" do
-    before(:each) { get :new }
-    it "renders the :new template" do
-      expect(response).to render_template :new
-    end
-    it "creates a new photo shoot" do
-      expect(assigns(:photo_shoot)).to be_a_new(PhotoShoot)
-    end
-  end
-
   describe "POST #create" do
-    context "with a valid photo shoot" do
-      it "redirects to the :index view" do
-        post :create, photo_shoot: valid_attribs
-        expect(response).to redirect_to photo_shoots_path
-      end
-      it "saves a photoshoot" do
-        expect{post :create, photo_shoot: valid_attribs}.to change{PhotoShoot.count}.by(1)
-      end
-      it "sets a flash :success message" do
-        post :create, photo_shoot: valid_attribs
-        expect(flash[:success]).to be_present
-      end
-    end
-
-    context "with an invalid photo shoot" do
-      it "redirects to the :new view" do
-        post :create, photo_shoot: invalid_attribs
-        expect(response).to render_template :new
-      end
-      it "does not save a photo shoot" do
-        expect{post :create, photo_shoot: invalid_attribs}.not_to change{PhotoShoot.count}
-      end
-      it "sets a flash :alert message" do
-        post :create, photo_shoot: invalid_attribs
-        expect(flash[:alert]).to be_present
-      end
-    end
-
     context "as JSON" do
       context "with valid parameters" do
         it "responds with a 201 code" do
@@ -93,16 +41,6 @@ describe PhotoShootsController do
   end
 
   describe "GET #show" do
-    context "with a valid id" do
-      before(:each) { get :show, id: id }
-      it "renders the :show view" do
-        expect(response).to render_template :show
-      end
-      it "retrieves a photo_shoot" do
-        expect(assigns(:photo_shoot)).to eq(photo_shoot)
-      end
-    end
-
     context "with an invalid id" do
       before(:each) do
         photo_shoot.delete
@@ -114,68 +52,10 @@ describe PhotoShootsController do
       it "sets flash alert message" do
         expect(flash[:alert]).to be_present
       end
-
-    end
-  end
-
-  describe "GET #edit" do
-    context "with a valid id" do
-      before(:each) { get :edit, id: id }
-      it "renders the :edit view" do
-        expect(response).to render_template :edit
-      end
-      it "retrieves a photo shoot" do
-        expect(assigns(:photo_shoot)).to eq(photo_shoot)
-      end
-    end
-
-    context "with an invalid id" do
-      before(:each) do
-        photo_shoot.delete
-        get :edit, id: id
-      end
-      it "redirects to the :index view" do
-        expect(response).to redirect_to photo_shoots_path
-      end
-      it "sets a flash alert message" do
-        expect(flash[:alert]).to be_present
-      end
     end
   end
 
   describe "PUT update" do
-    context "with valid attributes" do
-      it "redirects to the :index view" do
-        put :update, id: id, photo_shoot: second_valid_attribs
-        expect(response).to redirect_to photo_shoots_path
-      end
-      it "modifies the photo shoot" do
-        put :update, id: id, photo_shoot: second_valid_attribs
-        photo_shoot.reload
-        expect(photo_shoot.series).to eq("Another series")
-      end
-      it "sets a successful flash message" do
-        put :update, id: id, photo_shoot: second_valid_attribs
-        expect(flash[:success]).to be_present
-      end
-    end
-
-    context "with invalid attributes" do
-      it "renders the :edit view" do
-        put :update, id: id, photo_shoot: invalid_attribs
-        expect(response).to render_template :edit
-      end
-      it "does not modify the photo shoot" do
-        put :update, id: id, photo_shoot: invalid_attribs
-        photo_shoot.reload
-        expect(photo_shoot.convention_year_id).not_to be_nil
-      end
-      it "sets an alert flash message" do
-        put :update, id: id, photo_shoot: invalid_attribs
-        expect(flash[:alert]).to be_present
-      end
-    end
-
     context "as JSON" do
       before(:each) { photo_shoot }
       context "with valid parameters" do
