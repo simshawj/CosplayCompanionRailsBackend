9795e4d2e38fd15a1ba2cf95619e4c3de62ef5fa
James Simshaw <james.simshaw@gmail.com>
2016-08-15 17:31:00 -0700

    Removed Devise and files associated with ti

--- app/models/user.rb
+++
@@ -1,9 +0,0 @@
-class User < ApplicationRecord
-  validates :username, presence: true
-  validates_uniqueness_of :username, case_sensitive: false
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable,
-         :confirmable, :lockable
-end
--- spec/factories/users.rb
+++
@@ -1,7 +0,0 @@
-FactoryGirl.define do
-  factory :user do
-    username "johndoe"
-    email "john.doe@someplace.com"
-    password "Insecure"
-  end
-end
